2b1e2841193cdb4bfa1bf245c0eb7b4249a59700
Konstantin S. <havendv@gmail.com>
2026-06-12 23:59:09 +0400

    feat: Updated OpenAPI spec (#130)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

---
+++ src/libs/Runway/Generated/Runway.JsonConverters.CreateVideoToVideoRequestVariant1TargetAspectRatioNullable.g.cs
@@ -0,0 +1,60 @@
+#nullable enable
+
+namespace Runway.JsonConverters
+{
+    /// <inheritdoc />
+    public sealed class CreateVideoToVideoRequestVariant1TargetAspectRatioNullableJsonConverter : global::System.Text.Json.Serialization.JsonConverter<global::Runway.CreateVideoToVideoRequestVariant1TargetAspectRatio?>
+    {
+        /// <inheritdoc />
+        public override global::Runway.CreateVideoToVideoRequestVariant1TargetAspectRatio? Read(
+            ref global::System.Text.Json.Utf8JsonReader reader,
+            global::System.Type typeToConvert,
+            global::System.Text.Json.JsonSerializerOptions options)
+        {
+            switch (reader.TokenType)
+            {
+                case global::System.Text.Json.JsonTokenType.String:
+                {
+                    var stringValue = reader.GetString();
+                    if (stringValue != null)
+                    {
+                        return global::Runway.CreateVideoToVideoRequestVariant1TargetAspectRatioExtensions.ToEnum(stringValue);
+                    }
+                    
+                    break;
+                }
+                case global::System.Text.Json.JsonTokenType.Number:
+                {
+                    var numValue = reader.GetInt32();
+                    return (global::Runway.CreateVideoToVideoRequestVariant1TargetAspectRatio)numValue;
+                }
+                case global::System.Text.Json.JsonTokenType.Null:
+                {
+                    return default(global::Runway.CreateVideoToVideoRequestVariant1TargetAspectRatio?);
+                }
+                default:
+                    throw new global::System.ArgumentOutOfRangeException(nameof(reader));
+            }
+
+            return default;
+        }
+
+        /// <inheritdoc />
+        public override void Write(
+            global::System.Text.Json.Utf8JsonWriter writer,
+            global::Runway.CreateVideoToVideoRequestVariant1TargetAspectRatio? value,
+            global::System.Text.Json.JsonSerializerOptions options)
+        {
+            writer = writer ?? throw new global::System.ArgumentNullException(nameof(writer));
+
+            if (value == null)
+            {
+                writer.WriteNullValue();
+            }
+            else
+            {
+                writer.WriteStringValue(global::Runway.CreateVideoToVideoRequestVariant1TargetAspectRatioExtensions.ToValueString(value.Value));
+            }
+        }
+    }
+}
